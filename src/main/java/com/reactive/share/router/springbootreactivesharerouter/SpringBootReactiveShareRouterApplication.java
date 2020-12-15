4a002cb0f63d85d87166e92d2f33de957353fa4a
Florian <florian.martin63000@gmail.com>
2020-12-15 21:49:15 +0100

    Initial commit

---
+++ src/main/java/com/reactive/share/router/springbootreactivesharerouter/SpringBootReactiveShareRouterApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.share.router.springbootreactivesharerouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveShareRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveShareRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/share/router/springbootreactivesharerouter/SpringBootReactiveShareRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.share.router.springbootreactivesharerouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveShareRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
